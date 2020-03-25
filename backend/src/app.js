49601f838ef9b8453c2a122003ec03f861ebf9b7
Álefe Cruz <alefecrz@gmail.com>
2020-03-25 14:57:48 -0300

    fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,22 @@
 import express from 'express';
 
 import routes from './router';
+import './database';
 
-class app{
-  constructor(){
+class App {
+  constructor() {
     this.server = express();
     this.middleware();
     this.routes();
   }
-  middleware(){
+
+  middleware() {
     this.server.use(express.json());
   }
-  routes(){
+
+  routes() {
     this.server.use(routes);
   }
 }
 
-export default new app().server;
+export default new App().server;
