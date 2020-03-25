b77b8447877d85fbd1a46f438bf338ff52c5184e
Álefe Cruz <alefecrz@gmail.com>
2020-03-25 16:59:18 -0300

    Add routes

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 
 import OngController from './app/controllers/OngController';
+import IncidentController from './app/controllers/IncidentController';
 
 const routes = Router();
 
+routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.store);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.store);
+routes.delete('/incidents/:id', IncidentController.destroy);
 
 export default routes;
