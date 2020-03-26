cb48bd94c6188d9b7c3726de9d2db28475e781c5
Álefe Cruz <alefecrz@gmail.com>
2020-03-26 09:09:27 -0300

    Create Routes

---
+++ frontend/src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Register from './pages/Register'; 
+export default function Routes() {
+  return ( 
+    <BrowserRouter>
+      <Switch>
+      <Route path="/" exact component={Login}></Route>
+      <Route path="/register" component={Register}></Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
