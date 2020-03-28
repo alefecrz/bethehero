a81bd31383571ae0b57dbdcda319f22ff452d2c0
Álefe Cruz <alefecrz@gmail.com>
2020-03-28 09:33:00 -0300

    Verificação se ong está logada para listagem de incidents

--- backend/src/app/controllers/IncidentController.js
+++ backend/src/app/controllers/IncidentController.js
@@ -1,16 +1,20 @@
-import crypto from 'crypto';
 import Incidents from '../models/Incidents';
 
 class IncidentsController {
   async index(req, res) {
     const { page = 1 } = req.query;
+    const ong_id = req.headers.authorization;
 
+    if(!ong_id) {
+      return res.status(401).json({ error: "Operation not permitted."});
+    }
     const [incidentsCount] = await Incidents.query().count();
 
     const incidents = await Incidents.query()
       .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
       .limit(5)
       .offset((page - 1) * 5)
+      .where('ong_id', ong_id)
       .select([
         'incidents.*',
         'ongs.name',
@@ -26,9 +30,7 @@ class IncidentsController {
 
   async store(req, res) {
     const { title, description, value } = req.body;
-    const id = crypto.randomBytes(10).toString('HEX');
     const ong_id = req.headers.authorization;
-
     try {
       await Incidents.query().insert({
         title,
@@ -37,9 +39,9 @@ class IncidentsController {
         ong_id,
       });
 
-      return res.json({ id, title, description, value, ong_id });
+      return res.json({ title, description, value, ong_id });
     } catch (err) {
-      return res.status(err.statusCode).json({ error: err });
+      return res.status(401).json({ error: err });
     }
   }
 
